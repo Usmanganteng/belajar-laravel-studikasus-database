385011bcd23bf863326b40983b75de69f7619710
lucian666 <ilhamaldizar@gmail.com>
2024-05-03 00:48:38 +0700

    update file  app/Services/Impl/

--- app/Services/Impl/TodolistServiceImpl.php
+++ app/Services/Impl/TodolistServiceImpl.php
@@ -2,6 +2,7 @@
 
 namespace App\Services\Impl;
 
+use App\Models\Todo;
 use App\Services\TodolistService;
 use Illuminate\Support\Facades\Session;
 
@@ -10,32 +11,23 @@ class TodolistServiceImpl implements TodolistService
 
     public function saveTodo(string $id, string $todo): void
     {
-        if (!Session::exists("todolist")) {
-            Session::put("todolist", []);
-        }
-
-        Session::push("todolist", [
-            "id" => $id,
-            "todo" => $todo
-        ]);
+       $todo = new todo([
+        "id" => $id,
+        "todo" => $todo
+       ]);
+       $todo->save();
     }
 
     public function getTodolist(): array
     {
-        return Session::get("todolist", []);
+        return Todo::query()->get()->toArray();
     }
 
     public function removeTodo(string $todoId)
     {
-        $todolist = Session::get("todolist");
-
-        foreach ($todolist as $index => $value) {
-            if ($value['id'] == $todoId) {
-                unset($todolist[$index]);
-                break;
-            }
+        $todo = Todo::query()->find($todoId);
+        if($todo != null){
+            $todo->delete();
         }
-
-        Session::put("todolist", $todolist);
     }
 }
